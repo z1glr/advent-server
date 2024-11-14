0e65ed8e302ee1608a985848868a17fce114fd13
Simon Ziegler <simon.ziegler.1998+github@pm.me>
2024-11-14 01:14:14 +0100

    added missing files from last commit

---
+++ client/src/BaseAbout.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+	import BaseStatic from '@/BaseStatic.vue';
+
+</script>
+
+<template>
+	<BaseStatic>
+		<h1>About</h1>
+		<p>
+			Dies Seite ist begleitend zum Adventsangebot von Alex Unverhau.
+		</p>
+		<p>
+			Für weitere Informationen kontaktieren Sie <a href="mailto:advent@evkirchebuehl.de">advent@evkirchebuehl.de</a>.
+		</p>
+	</BaseStatic>	
+</template>
+
+<style scoped>
+	p {
+		margin-block: 0.55em;
+	}
+
+	p.container {
+		margin-block: 1em;
+	}
+
+	a {
+		color: var(--color-contrast);
+	}
+
+	h2, h3 {
+		margin-top: 1em;
+		margin-bottom: 0.25em;
+	}
+
+	ul {
+		margin-block: 1em;
+	}
+</style>
