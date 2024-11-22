4252ef624ec0e634d6e3e4e9c2d5cbf967132799
Simon Ziegler <simon.ziegler.1998+github@pm.me>
2024-11-22 22:32:13 +0100

    refactored basebutton

--- client/src/components/BaseButton.vue
+++ client/src/components/BaseButton.vue
@@ -6,53 +6,17 @@
 </script>
 
 <template>
-	<span class="button" :class="{ active, disabled }">
+	<span
+		class="justify-centent m-1 inline-flex aspect-square h-4 select-none items-center transition-colors hover:text-stone-900"
+		:class="{
+			'cursor-not-allowed text-stone-500 hover:text-stone-500': disabled,
+			'cursor-pointer': !disabled
+		}"
+	>
 		<div>
 			<slot></slot>
 		</div>
 	</span>
 </template>
 
-<style scoped>
-	.button {
-		display: inline-flex;
-		align-items: center;
-		justify-content: center;
-
-		user-select: none;
-
-		transition: background-color 0.1s ease;
-
-		aspect-ratio: 1;
-		height: 100%;
-		height: 1em;
-
-		margin: 0.25em;
-	}
-
-	.button:hover {
-		color: var(--color-text-hover);
-	}
-
-	.button.disabled {
-		color: var(--color-text-disabled);
-	}
-
-	.button.active,
-	.button:active:not(.disabled) {
-		color: var(--color-on);
-	}
-
-	.button.disabled.active {
-		color: var(--color-on-disabled);
-	}
-
-	.button.active:hover,
-	.button:active:hover:not(.disabled) {
-		color: var(--color-on-hover);
-	}
-
-	.button:not(.disabled) {
-		cursor: pointer;
-	}
-</style>
+<style scoped></style>
